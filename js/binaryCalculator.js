84faec10e1f72661721e67cb871c623273e7ea46
Pandya <vpandya@amazon.com>
2017-11-15 19:04:12 +0530

    Put some checks for 1-operation-at-a-time and input validity

--- js/binaryCalculator.js
+++ js/binaryCalculator.js
@@ -15,6 +15,11 @@ function addToResText (e) {
     var btn = e.target || e.srcElement;
     var str = document.getElementById(btn.id).innerHTML;
     resField.innerHTML += str;
+    if(hasOperationSymbol(str) && currentOperation !== null) {
+        window.alert("Multiple operations in one command is NOT supported. Retry.");
+        resetOnError();
+        return;
+    }
     if(str == '+')
         currentOperation = '+';
     if(str == '-')
@@ -33,17 +38,31 @@ btnMul.onclick = addToResText;
 btnDiv.onclick = addToResText;
 
 btnClr.onclick = function() {
+    /* same as resetOnError() */
     resField.innerHTML = "";
     currentOperation = null;
 }
 
 btnEql.onclick = function() {
     var resText = resField.innerHTML;
-    if(resText.length == 0) return;
-    if(currentOperation == null) return;
+    if(resText.length == 0 || currentOperation == null ) {
+        window.alert("Either given input is empty or without an operation symbol. Retry.");
+        resetOnError();
+        return; 
+    }
     var opPos = resText.indexOf(currentOperation);
     var num1Str = resText.substring(0, opPos);
     var num2Str = resText.substring(opPos+1, resText.length);
+    if(hasOperationSymbol(num1Str) || hasOperationSymbol(num2Str)) { /* safety check - addToResText() already has this */ 
+        window.alert("Multiple operations in one command is NOT supported. Retry.");
+        resetOnError();
+        return;
+    }
+    if(num1Str.length <= 0 || num2Str.length <= 0) {
+        window.alert("Please provide both the binary operands. Retry.");
+        resetOnError();
+        return;
+    }
     var num1Int = parseInt(num1Str, 2);
     var num2Int = parseInt(num2Str, 2);
     var res = 0;
@@ -56,4 +75,22 @@ btnEql.onclick = function() {
     if(currentOperation == '-')
         res = num1Int - num2Int;
     resField.innerHTML = res.toString(2);
+    currentOperation = null;
+}
+
+var hasOperationSymbol = function (str) {
+    if(typeof str !== "string" || str === undefined || str == null || str.length <= 0 )
+        return false;
+    var opArr = ['+', '-', '/', '*'];
+    for(let i = 0; i < opArr.length; i++) {
+        if(str.indexOf(opArr[i]) >= 0)
+            return true;
+    }
+    return false;
+}
+
+var resetOnError = function () {
+        resField.innerHTML = "";
+        currentOperation = null;
+        return;
 }
